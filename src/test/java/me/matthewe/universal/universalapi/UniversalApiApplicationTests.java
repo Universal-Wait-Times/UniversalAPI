617aaad60cbcf5f7d07663c8f8440dbac4ba4857
matt11matthew <matt11matthew@gmail.com>
2025-03-31 22:20:23 -0400

    Universal API

---
+++ src/test/java/me/matthewe/universal/universalapi/UniversalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.matthewe.universal.universalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
