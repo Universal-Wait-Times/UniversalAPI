1f916563facd429103da17fdfda08029771a5a4d
matt11matthew <matt11matthew@gmail.com>
2025-04-08 09:21:16 -0400

    Update AttractionSnapshotRepository.java

--- api/src/main/java/me/matthewe/universal/universalapi/v1/attractionservice/database/AttractionSnapshotRepository.java
+++ api/src/main/java/me/matthewe/universal/universalapi/v1/attractionservice/database/AttractionSnapshotRepository.java
@@ -1,5 +1,6 @@
 package me.matthewe.universal.universalapi.v1.attractionservice.database;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 public interface AttractionSnapshotRepository extends MongoRepository<AttractionSnapshot, String> {
-    AttractionSnapshot findTopByWaitTimeAttractionIdOrderByPulledAtDesc(String id);
 }
