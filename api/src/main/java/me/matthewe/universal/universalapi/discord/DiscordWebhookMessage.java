98de3efda354546b858c03f3db1a53596ffd6e4e
matt11matthew <matt11matthew@gmail.com>
2025-04-10 13:13:44 -0400

    Discord module

--- api/src/main/java/me/matthewe/universal/universalapi/discord/DiscordWebhookMessage.java
+++
@@ -1,8 +0,0 @@
-package me.matthewe.universal.universalapi.discord;
-
-import me.micartey.webhookly.DiscordWebhook;
-
-public class DiscordWebhookMessage {
-    private DiscordWebhook discordWebhook;
-    private DiscordWebhookMessage message;
-}
---
+++ discord/src/test/java/me/matthewe/universal/discord/DiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package me.matthewe.universal.discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
