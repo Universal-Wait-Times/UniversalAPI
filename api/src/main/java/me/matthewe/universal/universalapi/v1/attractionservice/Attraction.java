a0ce09f0cc3e052e519b1bccfb7fb810a26dc7e7
matt11matthew <matt11matthew@gmail.com>
2025-04-10 13:42:11 -0400

    Update Attraction.java

--- api/src/main/java/me/matthewe/universal/universalapi/v1/attractionservice/Attraction.java
+++ api/src/main/java/me/matthewe/universal/universalapi/v1/attractionservice/Attraction.java
@@ -148,7 +148,7 @@ public class Attraction {
         }
 
         public static enum Status {
-            BRIEF_DELAY, CLOSED, OPENS_AT, RIDE_NOW, OPEN,AT_CAPACITY, SPECIAL_EVENT, UNKNOWN, WEATHER_DELAY;
+            BRIEF_DELAY, CLOSED, OPENS_AT, RIDE_NOW, OPEN,AT_CAPACITY, SPECIAL_EVENT, UNKNOWN, WEATHER_DELAY, EXTENDED_CLOSURE ;
 
             @JsonCreator 
             public static Status fromString(String value) {
