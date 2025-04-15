8e7d09ac44cdc1b21deb5398b5bdc10e48e38b59
matt11matthew <matt11matthew@gmail.com>
2025-04-15 14:11:38 -0400

    Fixes

--- api/src/main/java/me/matthewe/universal/universalapi/MongoConfig.java
+++ api/src/main/java/me/matthewe/universal/universalapi/MongoConfig.java
@@ -4,11 +4,14 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.bson.codecs.jsr310.Jsr310CodecProvider;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.jsr310.Jsr310CodecProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
@@ -17,13 +20,23 @@ public class MongoConfig {
     public MongoClient mongoClient() {
         CodecRegistry defaultRegistry = MongoClientSettings.getDefaultCodecRegistry();
         CodecRegistry jsr310Registry = CodecRegistries.fromProviders(new Jsr310CodecProvider());
-        CodecRegistry combined = CodecRegistries.fromRegistries(defaultRegistry, jsr310Registry);
+        CodecRegistry combinedRegistry = CodecRegistries.fromRegistries(defaultRegistry, jsr310Registry);
 
         MongoClientSettings settings = MongoClientSettings.builder()
-            .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
-            .codecRegistry(combined)
-            .build();
+                .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
+                .codecRegistry(combinedRegistry)
+                .build();
 
         return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        return new SimpleMongoClientDatabaseFactory(mongoClient, "universalapi");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory) {
+        return new MongoTemplate(factory);
     }
 }
