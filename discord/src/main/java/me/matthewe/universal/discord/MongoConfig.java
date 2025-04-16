923864dde5b4df64acce62bdc56c9888a501c6f6
matt11matthew <matt11matthew@gmail.com>
2025-04-16 08:41:32 -0400

    Fixes

---
+++ discord/src/main/java/me/matthewe/universal/discord/MongoConfig.java
@@ -0,0 +1,42 @@
+package me.matthewe.universal.discord;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.jsr310.Jsr310CodecProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        CodecRegistry defaultRegistry = MongoClientSettings.getDefaultCodecRegistry();
+        CodecRegistry jsr310Registry = CodecRegistries.fromProviders(new Jsr310CodecProvider());
+        CodecRegistry combinedRegistry = CodecRegistries.fromRegistries(defaultRegistry, jsr310Registry);
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
+                .codecRegistry(combinedRegistry)
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        return new SimpleMongoClientDatabaseFactory(mongoClient, "universalapi");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory) {
+        return new MongoTemplate(factory);
+    }
+}
