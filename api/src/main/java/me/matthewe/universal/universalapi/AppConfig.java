885873e264fea5a2a62e62370cc339ab8bbdd2d9
matt11matthew <matt11matthew@gmail.com>
2025-05-23 22:34:38 -0400

    Update AppConfig.java

--- api/src/main/java/me/matthewe/universal/universalapi/AppConfig.java
+++ api/src/main/java/me/matthewe/universal/universalapi/AppConfig.java
@@ -4,6 +4,8 @@ import me.matthewe.universal.universalapi.v1.redis.RedisPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -11,7 +13,19 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("*") // Allow all HTTP methods (GET, POST, etc.)
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
 //
 //    @Bean
 //    public RedisPublisher redisPublisher() {
