613d23eb9943482962e475a5f598d23c4224d3d7
matt11matthew <matt11matthew@gmail.com>
2025-05-23 22:44:52 -0400

    Update TicketWebPageController.java

--- api/src/main/java/me/matthewe/universal/universalapi/v1/ticketdata/TicketWebPageController.java
+++ api/src/main/java/me/matthewe/universal/universalapi/v1/ticketdata/TicketWebPageController.java
@@ -1,16 +1,22 @@
 package me.matthewe.universal.universalapi.v1.ticketdata;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("/tickets")
 public class TicketWebPageController {
 
-    @GetMapping
-    public RedirectView ticketWebPage() {
-        return new RedirectView("/ticketPage.html");
+    @GetMapping(produces = "text/html; charset=UTF-8")
+    public String ticketWebPage() throws IOException {
+        ClassPathResource htmlFile = new ClassPathResource("static/ticketPage.html");
+        return StreamUtils.copyToString(htmlFile.getInputStream(), StandardCharsets.UTF_8);
     }
 }
