4ad22f4aa02bb4c912b343f4159fd0be1f72a242
matt11matthew <matt11matthew@gmail.com>
2025-05-23 22:23:32 -0400

    Fixes

--- api/src/main/java/me/matthewe/universal/universalapi/UniversalApiApplication.java
+++ api/src/main/java/me/matthewe/universal/universalapi/UniversalApiApplication.java
@@ -2,14 +2,23 @@ package me.matthewe.universal.universalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableCaching
 public class UniversalApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniversalApiApplication.class, args);
     }
-
+    @Bean
+    public CacheManager cacheManager() {
+        // names must match what you use in @Cacheable(...)
+        return new ConcurrentMapCacheManager("sortedTickets");
+    }
 }
